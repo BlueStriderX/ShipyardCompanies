b67d7af9315d4fec533cc85ec1d1503d6e9fd643
TheDerpGamer <garret2kool@gmail.com>
2020-06-22 12:20:09 -0700

    Update NewFactionPanel.java

--- src/net/soe/shipyardcompanies/gui/NewFactionPanel.java
+++ src/net/soe/shipyardcompanies/gui/NewFactionPanel.java
@@ -1,5 +1,8 @@
 package net.soe.shipyardcompanies.gui;
 
+import api.faction.Faction;
+import net.soe.shipyardcompanies.shipyards.CompanyTechFocus;
+import net.soe.shipyardcompanies.shipyards.DesignCompany;
 import org.schema.game.client.view.gui.faction.newfaction.FactionPanelNew;
 import org.schema.schine.graphicsengine.forms.font.FontLibrary;
 import org.schema.schine.graphicsengine.forms.gui.GUITextOverlay;
@@ -29,5 +32,17 @@ public class NewFactionPanel extends FactionPanelNew {
         this.designCompaniesTab.addNewTextBox(10);
         textOverlay.setPos(4.0F, 4.0F, 0.0F);
         this.designCompaniesTab.getContent(0).attach(textOverlay);
+
+        //Debug Testing
+        String testDescription = "A Dovan design company focused on energy and electronics. They are well known for their computer terminals and DovOS.";
+        DesignCompany testCompany = new DesignCompany(new Faction(this.getOwnFaction()), "DovTech", testDescription, CompanyTechFocus.ENERGY);
+        String companyText = testCompany.getName() + "\n" + testCompany.getDescription() + "\n" + testCompany.getTechFocus().getDisplayName() + "\n" + testCompany.getTechFocus().getDescription();
+
+
+        GUITextOverlay testCompanyOverlay;
+        (testCompanyOverlay = new GUITextOverlay(10, 10, FontLibrary.getBlenderProMedium20(), this.getState())).setTextSimple(companyText);
+        this.designCompaniesTab.addNewTextBox(85);
+        testCompanyOverlay.setPos(4.0F, 4.0F, 0.0F);
+        this.designCompaniesTab.getContent(1).attach(testCompanyOverlay);
     }
 }
