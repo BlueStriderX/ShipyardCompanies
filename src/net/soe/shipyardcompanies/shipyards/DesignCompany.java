5d95dbbf274e36f92dbdff7509bc792f3c5bf98a
TheDerpGamer <garret2kool@gmail.com>
2020-06-22 12:20:20 -0700

    Update DesignCompany.java

--- src/net/soe/shipyardcompanies/shipyards/DesignCompany.java
+++ src/net/soe/shipyardcompanies/shipyards/DesignCompany.java
@@ -6,4 +6,41 @@ public class DesignCompany {
 
     private Faction ownerFaction;
     private String name;
+    private String description;
+    private CompanyTechFocus techFocus;
+
+    public DesignCompany(Faction ownerFaction, String name, String description, CompanyTechFocus techFocus) {
+        this.ownerFaction = ownerFaction;
+        this.name = name;
+        this.description = description;
+        this.techFocus = techFocus;
+    }
+
+    public Faction getOwnerFaction() {
+        return ownerFaction;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public CompanyTechFocus getTechFocus() {
+        return techFocus;
+    }
+
+    public void setTechFocus(CompanyTechFocus techFocus) {
+        this.techFocus = techFocus;
+    }
 }
