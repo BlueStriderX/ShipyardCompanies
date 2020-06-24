53b1bab7b5366635a4e4b120cc68abe021ab6ec4
TheDerpGamer <garret2kool@gmail.com>
2020-06-24 10:29:47 -0700

    Update ShipyardCompanies.java

--- src/net/soe/shipyardcompanies/ShipyardCompanies.java
+++ src/net/soe/shipyardcompanies/ShipyardCompanies.java
@@ -2,23 +2,24 @@ package net.soe.shipyardcompanies;
 
 import api.DebugFile;
 import api.listener.Listener;
-import api.listener.events.Event;
 import api.listener.events.gui.ControlManagerActivateEvent;
 import api.mod.StarLoader;
 import api.mod.StarMod;
-import net.soe.shipyardcompanies.gui.NewFactionPanel;
 import net.soe.shipyardcompanies.gui.NewShopPanel;
+import net.soe.shipyardcompanies.shipyards.ShipyardCompany;
 import org.schema.game.client.data.GameClientState;
 import org.schema.game.client.view.gui.PlayerPanel;
-import org.schema.game.client.view.gui.faction.newfaction.FactionPanelNew;
 import org.schema.game.client.view.gui.shop.shopnew.ShopPanelNew;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class ShipyardCompanies extends StarMod {
     static ShipyardCompanies inst;
     public ShipyardCompanies() {
         inst = this;
     }
+    private ArrayList<ShipyardCompany> companies = new ArrayList<ShipyardCompany>();
+    private boolean debug;
 
     public static void main(String[] args) {
 
@@ -28,7 +29,7 @@ public class ShipyardCompanies extends StarMod {
     public void onGameStart() {
         this.modName = "ShipyardCompanies";
         this.modAuthor = "DovTech";
-        this.modVersion = "0.1.0";
+        this.modVersion = "0.1.2";
         this.modDescription = "Adds new functionality for shops and shipyards.";
     }
 
@@ -36,40 +37,55 @@ public class ShipyardCompanies extends StarMod {
     public void onEnable() {
         super.onEnable();
         DebugFile.log("Enabled", this);
-        registerListeners();
-    }
-
-    private void registerListeners() {
+        debug = true;
 
         //GUI Modifiers
-        StarLoader.registerListener(ControlManagerActivateEvent.class, new Listener() {
+        StarLoader.registerListener(ControlManagerActivateEvent.class, new Listener<ControlManagerActivateEvent>() {
             @Override
-            public void onEvent(Event e) {
+            public void onEvent(ControlManagerActivateEvent event) {
                 PlayerPanel playerPanel = GameClientState.instance.getWorldDrawer().getGuiDrawer().getPlayerPanel();
-                playerPanel.onInit();
-                GameClientState state = playerPanel.getState();
-                try {
-                    //Faction Panel Design Companies Tab
-                    Field designCompaniesPanelField = PlayerPanel.class.getDeclaredField("factionPanelNew");
-                    designCompaniesPanelField.setAccessible(true);
-                    FactionPanelNew factionPanelNew = (FactionPanelNew) designCompaniesPanelField.get(playerPanel);
-                    if(!(factionPanelNew instanceof NewFactionPanel)) {
-                        designCompaniesPanelField.set(playerPanel, new NewFactionPanel(state));
-                    }
+                if(event.isActive()) {
+                    if (debug) DebugFile.log("[DEBUG]: ControlManagerActivateEvent Fired");
+                    try {
+                        /*
+                        //Faction Panel Design Companies Tab
+                        Field designCompaniesPanelField = PlayerPanel.class.getDeclaredField("factionPanelNew");
+                        designCompaniesPanelField.setAccessible(true);
+                        FactionPanelNew factionPanelNew = (FactionPanelNew) designCompaniesPanelField.get(playerPanel);
+                        if (!(factionPanelNew instanceof NewFactionPanel)) {
+                            GameClientState state = playerPanel.getState();
+                            designCompaniesPanelField.set(playerPanel, new NewFactionPanel(state));
+                            if (debug) DebugFile.log("[DEBUG]: Swapped out FactionPanelNew");
+                        }
+                         */
 
-                    //Shop Panel Shipyards Tab
-                    Field shipyardsPanelField = PlayerPanel.class.getDeclaredField("shopPanelNew");
-                    shipyardsPanelField.setAccessible(true);
-                    ShopPanelNew shopPanelNew = (ShopPanelNew) shipyardsPanelField.get(playerPanel);
-                    if(!(shopPanelNew instanceof NewShopPanel)) {
-                        shipyardsPanelField.set(playerPanel, new NewShopPanel(state));
+                        //Shop Panel Shipyards Tab
+                        Field shipyardsPanelField = PlayerPanel.class.getDeclaredField("shopPanelNew");
+                        shipyardsPanelField.setAccessible(true);
+                        ShopPanelNew shopPanelNew = (ShopPanelNew) shipyardsPanelField.get(playerPanel);
+                        if (!(shopPanelNew instanceof NewShopPanel)) {
+                            GameClientState state = playerPanel.getState();
+                            shipyardsPanelField.set(playerPanel, new NewShopPanel(state));
+                            if (debug) DebugFile.log("[DEBUG]: Swapped out ShopPanelNew");
+
+                        }
+                    } catch (NoSuchFieldException ex) {
+                        ex.printStackTrace();
+                    } catch (IllegalAccessException ex) {
+                        ex.printStackTrace();
+                    } catch (NullPointerException ex) {
+                        ex.printStackTrace();
                     }
-                } catch (NoSuchFieldException ex) {
-                    ex.printStackTrace();
-                } catch (IllegalAccessException ex) {
-                    ex.printStackTrace();
                 }
             }
         });
+    }
+
+    public static ShipyardCompanies getInst() {
+        return inst;
+    }
+
+    public ArrayList<ShipyardCompany> getCompanies() {
+        return companies;
     }
 }
